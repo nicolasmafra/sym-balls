cb72864b425045489b825bf14dc6d466f2dbc698
Nicolas Mafra <nickmafra@hotmail.com>
2023-01-01 14:59:42 -0300

    add 3D rendering example

--- src/js/Gfx.mjs
+++ src/js/Gfx.mjs
@@ -12,12 +12,56 @@ export default {
 
     configure: function() {
         this.aspectRatio = window.innerWidth / window.innerHeight;
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.camera = new THREE.PerspectiveCamera(this.fieldOfView, this.aspectRatio, this.near, this.far);
+        this.camera.position.z = 5;
 
         document.body.appendChild(this.renderer.domElement);
     },
 
     start: function() {
+
+        this.addLights();
+
+        this.addSampleObject();
+
+        this.loop();
+    },
+
+    addLights: function() {
+        const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
+        dirLight.position.set(1, 2, 1);
+        dirLight.target.position.set(0, 0, 0);
+        this.scene.add(dirLight);
+        this.scene.add(dirLight.target);
+
+        const ambLight = new THREE.AmbientLight(0xffffff, 0.1);
+        this.scene.add(ambLight);
+    },
+
+    loop: function() {
+        this.update();
+
+        requestAnimationFrame( () => this.loop() );
+    },
+
+    update: function() {
+
+        this.updateSampleObject();
+        
         this.renderer.render(this.scene, this.camera);
+    },
+
+    addSampleObject: function() {
+        const geometry = new THREE.SphereGeometry( 1, 16, 8 );
+        const material = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
+        this.obj = new THREE.Mesh(geometry, material);
+
+        this.scene.add(this.obj);
+    },
+
+    updateSampleObject: function() {
+        this.obj.rotation.x += 0.01;
+        this.obj.rotation.y += 0.01;
     },
 }
