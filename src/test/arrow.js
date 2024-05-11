a6f4bc8e25fcf760cf41d03c2c3275b045f88939
Nicolas Mafra <nickmafra@hotmail.com>
2024-05-11 20:03:28 -0300

    temp: interrompido pela esposa

--- src/test/arrow.js
+++ src/test/arrow.js
@@ -13,38 +13,50 @@ function createArrowPart(color, dest, radius) {
 }
 
 function calcCenter(curveAngle) {
-    return curveAngle == 0 ? 0 : (1 / Math.tan(curveAngle));
+    if (curveAngle < Math.PI/2) {
+        
+    } else {
+        return 1 / Math.tan(curveAngle);
+    }
+}
+
+function lerp(start, end, progress) {
+    return start + (end - start) * progress;
 }
 
 class Arrow {
-
-    partRadius = 10;
     
-    constructor(colorOrigin, colorDestination, curveAngle=0) {
-        this.curveAngle = curveAngle;
-        this.centerDist = this.partRadius * calcCenter(curveAngle);
-
+    constructor(colorOrigin, colorDestination, curveAngle, partRadius) {
         this.gfx = new PIXI.Container();
         this.gfx.scale.x = 2;
         this.gfx.scale.y = 2;
     
-        this.origin = createArrowPart(colorOrigin, false, this.partRadius);
+        this.origin = createArrowPart(colorOrigin, false, partRadius);
         this.origin.x = -10;
-        this.origin.y = -this.centerDist;
-        this.origin.rotation = -curveAngle;
         this.gfx.addChild(this.origin);
     
-        this.destination = createArrowPart(colorDestination, true, this.partRadius);
+        this.destination = createArrowPart(colorDestination, true, partRadius);
         this.destination.x = 10;
-        this.destination.y = -this.centerDist;
-        this.destination.rotation = curveAngle;
         this.gfx.addChild(this.destination);
+
+        this.setCurveAngle(curveAngle, partRadius);
+    }
+
+    setCurveAngle(curveAngle, partRadius) {
+        this.curveAngle = curveAngle;
+        let centerDist = partRadius * calcCenter(curveAngle);
+        this.origin.y = -centerDist;
+        this.origin.rotation = -curveAngle;
+        this.destination.y = -centerDist;
+        this.destination.rotation = curveAngle;
     }
 }
 
 class ArrowGroup {
 
+    partRadius = 10;
     arrows = [];
+    animationProgress = 1;
     
     constructor(colors) {
         this.colors = colors;
@@ -53,10 +65,37 @@ class ArrowGroup {
         for (let i = 0; i < colors.length; i++) {
             let color1 = colors[i];
             let color2 = colors[(i + 1) % colors.length];
-            let arrow = new Arrow(color1, color2, curveAngle);
+            let arrow = new Arrow(color1, color2, curveAngle, this.partRadius);
             arrow.gfx.rotation = 2 * curveAngle * i;
             this.arrows.push(arrow);
             this.gfx.addChild(arrow.gfx);
         }
+    }
+
+    animateToLine(duration=1000) {
+        this.animationProgress = 0;
+        this.animationDuration = duration;
+
+        let spacing = 2 * this.partRadius;
+        let offsetY = -spacing * (this.arrows.length-1)/2;
+        for (let i = 0; i < this.arrows.length; i++) {
+            let arrow = this.arrows[i];
+            
+            arrow.targetY = offsetY + spacing * i;
+            arrow.targetRotation = 0;
+            arrow.targetCurveAngle = 0;
+        }
+    }
+
+    update(deltaTime) {
+        if (this.animationProgress == 1) return;
+
+        this.animationProgress = Math.min(this.animationProgress + deltaTime / this.animationDuration, 1);
+        
+        this.arrows.forEach(arrow => {
+            arrow.gfx.y = lerp(arrow.gfx.y, arrow.targetY, this.animationProgress);
+            arrow.gfx.rotation = lerp(arrow.gfx.rotation, arrow.targetRotation, this.animationProgress);
+            arrow.setCurveAngle(lerp(arrow.curveAngle, arrow.targetCurveAngle, this.animationProgress), this.partRadius);
+        });
     }
 }
