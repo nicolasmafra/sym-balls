3716f9ef271966d4c30eb554a62d6d679780c7ab
Nicolas Mafra <nickmafra@hotmail.com>
2024-09-18 19:02:22 -0300

    Updates

--- js/core/LevelLoader.mjs
+++ js/core/LevelLoader.mjs
@@ -3,9 +3,9 @@ export default {
     levels: {},
 
     async configure() {
-        await this.fetch('w0', '../assets/levels-w0.json');
-        await this.fetch('w1', '../assets/levels-w1.json');
-        await this.fetch('wtest', '../assets/levels-wtest.json');
+        await this.fetch('w0', 'assets/levels-w0.json');
+        await this.fetch('w1', 'assets/levels-w1.json');
+        await this.fetch('wtest', 'assets/levels-wtest.json');
     },
 
     async fetch(world, worldPath) {
