1bb7f330d8047fd8efef444f57f97d820069525f
Nicolas Mafra <nickmafra@hotmail.com>
2025-01-23 21:50:56 -0300

    feat: use semantic tags

--- src/js/gfx/Gfx.mjs
+++ src/js/gfx/Gfx.mjs
@@ -17,7 +17,6 @@ export default {
     startTime: null,
     lastUpdate: null,
     totalTime: null,
-    domContainer: null,
     requestAnimationFrameId: null,
     started: false,
 
@@ -48,18 +47,19 @@ export default {
     },
 
     configure() {
+        const canvas = document.getElementById("game-canvas");
+
         this.configured = true;
-        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        this.renderer = new THREE.WebGLRenderer({
+            canvas,
+            antialias: true,
+        });
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(this.fieldOfView, this.aspectRatio, this.near, this.far);
         
         this.calculateCameraDistance();
         this.camera.position.z = this.cameraDistance;
-
-        if (!this.domContainer) this.domContainer = document.body;
-
-        this.domContainer.appendChild(this.renderer.domElement);
 
         this.resize();
 
