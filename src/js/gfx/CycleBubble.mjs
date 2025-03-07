0d695f1bed083d3edf8604808f335ea3285f2191
Nicolas Mafra <nickmafra@hotmail.com>
2025-03-07 19:16:12 -0300

    feat: add asymmetry to visual

--- src/js/gfx/CycleBubble.mjs
+++ src/js/gfx/CycleBubble.mjs
@@ -5,7 +5,12 @@ import Params from '../Params.mjs';
 import * as THREE from 'three';
 import BubbleUtils from './BubbleUtils.mjs';
 
-const ballGeometry = new THREE.SphereGeometry(1, 16, 8);
+const angle = 2;
+const partDistance = 0.05;
+const radius = 1 - 2*partDistance;
+
+const ballGeometry1 = new THREE.SphereGeometry(radius, 16, 8, -angle/2, angle);
+const ballGeometry2 = new THREE.SphereGeometry(radius, 16, 8, angle/2, 2*Math.PI - angle);
 
 const cycleMargin = 0.5;
 const bubbleRotationSpeed = 0.25;
@@ -15,7 +20,21 @@ const cycleXRotationSpeed = 0.5;
 let CycleBubbleBuilder = {
 
     indexToBall(i) {
-        return BubbleUtils.indexToMesh(i, ballGeometry);
+        let group = new THREE.Group();
+
+        let part1 = BubbleUtils.indexToMesh(i, ballGeometry1);
+        part1.rotation.x = Math.PI / 2;
+        part1.rotation.y = Math.PI / 2;
+        part1.position.y = -partDistance;
+        group.add(part1);
+
+        let part2 = BubbleUtils.indexToMesh(i, ballGeometry2);
+        part2.rotation.x = Math.PI / 2;
+        part2.rotation.y = Math.PI / 2;
+        part2.position.y = partDistance;
+        group.add(part2);
+
+        return group;
     },
 
     cycleToGroup(cycle) {
