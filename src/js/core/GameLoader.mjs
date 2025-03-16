328422072126c6fa2b0853e02a87b4c4dffe8c13
Nicolas Mafra <nickmafra@hotmail.com>
2025-03-16 20:38:20 -0300

    feat: add lab mode

--- src/js/core/GameLoader.mjs
+++ src/js/core/GameLoader.mjs
@@ -17,10 +17,12 @@ const GameLoader = {
             initialItems: parse(levelSchema.initialItems),
             generatingSet: parse(levelSchema.generatingSet),
             lockInitialItems: hasDock,
+            allowUseDockWithoutMerge: levelSchema.allowUseDockWithoutMerge || levelSchema.initialItems.length == 0,
             allowedDeletion: false,
             allowedDuplication: false,
             allowedInversion: false,
             finiteDock: levelSchema.finiteDock || false,
+            infinite: levelSchema.infinite || levelSchema.initialItems.length == 0,
         };
     },
 
