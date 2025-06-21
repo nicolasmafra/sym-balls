0d1020bfd8e0754bd5dc2fa732e5f97ce18b446f
Nicolas Mafra <nickmafra@hotmail.com>
2025-06-21 00:51:19 -0300

    fix resolution bugs

--- global_vars.gd
+++ global_vars.gd
@@ -3,7 +3,7 @@ extends Node
 var scene_stack := []
 
 const glyphs_to_export = "⮌⭯∞⮺⌫⑊«»0123456789abdcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ -_.,!?"
-const default_item_radius := 50.0
+const default_item_radius := 25.0
 var item_radius := default_item_radius
 var course_world_index := 1
 var course_level_index := -1
