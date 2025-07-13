7182b5edb83e032c5341f8d704cb65211a398329
Nicolas Mafra <nickmafra@hotmail.com>
2025-07-13 01:07:53 -0300

    Deploy build/web

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752346354|1437723631';
+const CACHE_VERSION = '1752379669|34753096937';
 /** @type {string} */
 const CACHE_PREFIX = 'sym-balls-godot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
